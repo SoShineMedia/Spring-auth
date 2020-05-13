979cef3ff68c78f670a42631dad35d9a2aac93ea
KenroyWhite <whitekenroy@yahoo.com>
2020-05-13 00:26:23 -0400

    update controllers

--- src/main/java/com/soshinemedia/auth/web/CaseController.java
+++ src/main/java/com/soshinemedia/auth/web/CaseController.java
@@ -5,11 +5,9 @@ import com.soshinemedia.auth.domain.Firm;
 import com.soshinemedia.auth.repository.CaseRepository;
 import com.soshinemedia.auth.repository.FirmRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -26,6 +24,141 @@ public class CaseController {
 
     @GetMapping("")
     public ResponseEntity<List<Case>> all(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @GetMapping("/{id}/bills/invoices")
+    public ResponseEntity<List<Case>> getInvoices(@RequestParam(name = "from", required = false) Date from, @RequestParam(name = "to", required = false) Date to, @PathVariable(value = "id", required = true) String id) {
+        if (from == null ) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PostMapping("/{id}/bills/invoices")
+    public ResponseEntity<List<Case>> saveInvoices(@RequestParam(name = "from", required = false) Date from, @RequestParam(name = "to", required = false) Date to, @PathVariable(value = "id", required = true) String id) {
+        if (from == null ) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PutMapping("/{id}/bills/invoices")
+    public ResponseEntity<List<Case>> updateInvoices(@RequestParam(name = "from", required = false) Date from, @RequestParam(name = "to", required = false) Date to, @PathVariable(value = "id", required = true) String id) {
+        if (from == null ) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @GetMapping("/{id}/bills/payments")
+    public ResponseEntity<List<Case>> getPayments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PostMapping("/{id}/bills/payments")
+    public ResponseEntity<List<Case>> savePayments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PutMapping("/{id}/bills/payments")
+    public ResponseEntity<List<Case>> updatePayments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @GetMapping("/{id}/documents")
+    public ResponseEntity<List<Case>> getDocuments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PostMapping("/{id}/documents")
+    public ResponseEntity<List<Case>> saveDocuments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PutMapping("/{id}/documents")
+    public ResponseEntity<List<Case>> updateDocuments(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @GetMapping("/{id}/reports")
+    public ResponseEntity<List<Case>> getReports(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PostMapping("/{id}/reports")
+    public ResponseEntity<List<Case>> saveReports(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PutMapping("/{id}/reports")
+    public ResponseEntity<List<Case>> updateReports(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @GetMapping("/{id}/events")
+            public ResponseEntity<List<Case>> getEvents(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PostMapping("/{id}/events")
+    public ResponseEntity<List<Case>> saveEvents(@RequestParam(name = "brand", required = false) String[] brands) {
+        if (brands == null || brands.length == 0) {
+            return ok(this.cases.findAll());
+        } else {
+
+            return ok(this.cases.findAll());
+        }
+    }
+    @PutMapping("/{id}/events")
+    public ResponseEntity<List<Case>> updateEvents(@RequestParam(name = "brand", required = false) String[] brands) {
         if (brands == null || brands.length == 0) {
             return ok(this.cases.findAll());
         } else {
--- src/main/java/com/soshinemedia/auth/web/UserinfoController.java
+++ src/main/java/com/soshinemedia/auth/web/UserinfoController.java
@@ -1,14 +1,13 @@
 package com.soshinemedia.auth.web;
 
+import com.soshinemedia.auth.domain.Case;
 import com.soshinemedia.auth.domain.Firm;
 import com.soshinemedia.auth.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,10 +17,13 @@ import static java.util.stream.Collectors.toList;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController()
+@RequestMapping("/v1/users")
 public class UserinfoController {
 
+    private List<Case> cases;
+
     @SuppressWarnings("rawtypes")
-	@GetMapping("/me")
+	@GetMapping("/{id}")
     public ResponseEntity currentUser(@AuthenticationPrincipal UserDetails userDetails){
         Map<Object, Object> model = new HashMap<>();
         model.put("username", userDetails.getUsername());
@@ -31,6 +33,75 @@ public class UserinfoController {
             .collect(toList())
         );
         return ok(model);
+    }
+
+    @GetMapping("/{id}/casess")
+    public ResponseEntity cases(@AuthenticationPrincipal UserDetails userDetails,@RequestParam(name = "brand", required = false) String[] brands) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(a -> ((GrantedAuthority) a).getAuthority())
+                .collect(toList())
+        );
+
+        if (brands == null || brands.length == 0) {
+            return ok(model);
+        } else {
+
+            return ok(model);
+        }
+    }
+    @GetMapping("/{id}/schedules")
+    public ResponseEntity getSchedules(@AuthenticationPrincipal UserDetails userDetails, @RequestParam(name = "brand", required = false) String[] brands) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(a -> ((GrantedAuthority) a).getAuthority())
+                .collect(toList())
+        );
+
+        if (brands == null || brands.length == 0) {
+            return ok(model);
+        } else {
+
+            return ok(model);
+        }
+    }
+    @PostMapping("/{id}/schedules")
+    public ResponseEntity Saveschedules(@AuthenticationPrincipal UserDetails userDetails, @RequestParam(name = "brand", required = false) String[] brands) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(a -> ((GrantedAuthority) a).getAuthority())
+                .collect(toList())
+        );
+
+        if (brands == null || brands.length == 0) {
+            return ok(model);
+        } else {
+
+            return ok(model);
+        }
+    }
+    @PutMapping("/{id}/schedules")
+    public ResponseEntity updateSchedules(@AuthenticationPrincipal UserDetails userDetails, @RequestParam(name = "brand", required = false) String[] brands) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(a -> ((GrantedAuthority) a).getAuthority())
+                .collect(toList())
+        );
+
+        if (brands == null || brands.length == 0) {
+            return ok(model);
+        } else {
+
+            return ok(model);
+        }
     }
 
 }
